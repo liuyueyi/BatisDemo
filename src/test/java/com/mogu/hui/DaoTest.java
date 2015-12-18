6c6cc1d5dea5c6cf8a81accb6581b6e0989dea8b
yihui <yihui@mogujie.com>
2015-12-19 00:19:37 +0800

    1.1.0 batis with spring

--- src/test/java/com/mogu/hui/DaoTest.java
+++ src/test/java/com/mogu/hui/DaoTest.java
@@ -1,40 +1,36 @@
 package com.mogu.hui;
 
+import com.mogu.hui.dao.UserDao;
 import com.mogu.hui.domain.User;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
-import java.io.Reader;
 
 /**
  * Created by yihui on 15/12/18.
  */
 public class DaoTest {
-    private static SqlSessionFactory sqlSessionFactory;
-    private static Reader reader;
+    private static ApplicationContext ctx;
+    private UserDao userDao;
+
 
     @Before
     public void init() throws IOException {
-        reader = Resources.getResourceAsReader("mybatis.xml");
-        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-
+        ctx = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        userDao = ctx.getBean("userDao", UserDao.class);
     }
 
     @Test
     public void testDao() {
-        SqlSession session = sqlSessionFactory.openSession();
         try {
-            User user = session.selectOne("com.mogu.hui.dao.UserDao.getUserById", 1);
+            User user =  userDao.getUserById(1);
             System.out.println(user.toString());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
-            session.close();
         }
     }
 }
